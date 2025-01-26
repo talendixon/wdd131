b3531450b33511c3b23f7e3901abf601ecc981af
talendixon <talend8@byui.edu>
2025-01-25 17:07:21 -0700

    idk

---
+++ cool-pics/coolpics.js
@@ -0,0 +1,7 @@
+// JavaScript for menu button functionality on small screens
+document.querySelector('.menu-button').addEventListener('click', function () {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('hidden');
+  });
+  // JavaScript placeholder - Add functionality as needed
+console.log("Cool Pics loaded!");
